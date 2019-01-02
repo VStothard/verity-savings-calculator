d7540c4aa78cd867f17ae314f490313cbcd663b7
Verity Stothard <vjstothard@gmail.com>
2019-01-02 19:44:09 +1100

    set up nuxt and expense section component

---
+++ components/ExpenseGroup/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <!-- <ExpenseRow /> -->
+        hello {{expenseArray}}
+        <!-- <div v-for="(item, i) in expenses" :key="i">
+            {{item.itemName}}
+        </div> -->
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        expenseArray: {
+            type: Array
+        }
+    },
+    computed: {
+        expenses() {
+            return this.expenseArray
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="container">
+    <div>
+      <ExpenseGroup :expense-array="everydayExpenses" />
+      <ExpenseGroup :expense-array="funExpenses" />
+    </div>
+  </section>
+</template>
+
+<script>
+import ExpenseGroup from '~/components/ExpenseGroup'
+
+export default {
+  components: {
+    ExpenseGroup
+  },
+  data() {
+        return {
+            everydayExpenses: [
+              {
+                    itemName: 'first treat',
+                    itemDescription: 'the first thing',
+                    value: 2000
+                }, 
+              {
+                    itemName: 'first treat',
+                    itemDescription: 'the first thing',
+                    value: 2000
+                },
+            ],
+            funExpenses: [
+                {
+                    itemName: 'first treat',
+                    itemDescription: 'the first thing',
+                    value: 2000
+                },
+                {
+                    itemName: 'second treat',
+                    itemDescription: 'the second thing',
+                    value: 2000
+                },
+                
+            ]
+        }
+    }
+}
+</script>
